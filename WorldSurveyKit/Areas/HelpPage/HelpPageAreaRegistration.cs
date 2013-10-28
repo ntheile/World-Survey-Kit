f313c86f5e4f32a11e8184b627bbad6825fc472d
Nick Theile <u402046@.dow.com>
2013-10-28 15:56:04 -0400

    removed help route

--- WorldSurveyKit/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ WorldSurveyKit/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -15,10 +15,10 @@ namespace WorldSurveyKit.Areas.HelpPage
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
-            context.MapRoute(
-                "HelpPage_Default",
-                "Help/{action}/{apiId}",
-                new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
+            //context.MapRoute(
+            //    "HelpPage_Default",
+            //    "Help/{action}/{apiId}",
+            //    new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
 
             HelpPageConfig.Register(GlobalConfiguration.Configuration);
         }
