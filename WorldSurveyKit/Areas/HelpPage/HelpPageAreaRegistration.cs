ae7c3aed70375fabfe38bc6e80a5353c3c7206a3
Nick Theile <u402046@.dow.com>
2013-10-28 15:57:45 -0400

    removed help route 2

--- WorldSurveyKit/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ WorldSurveyKit/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -20,7 +20,7 @@ namespace WorldSurveyKit.Areas.HelpPage
             //    "Help/{action}/{apiId}",
             //    new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
 
-            HelpPageConfig.Register(GlobalConfiguration.Configuration);
+            //HelpPageConfig.Register(GlobalConfiguration.Configuration);
         }
     }
 }
