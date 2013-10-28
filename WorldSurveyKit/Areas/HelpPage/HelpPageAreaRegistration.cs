90d4e146ceff99b5be3dc2bf7e13f4543bb30bea
Nick Theile <u402046@.dow.com>
2013-10-28 16:05:05 -0400

    updated ApiDoc.Xml

--- WorldSurveyKit/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ WorldSurveyKit/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -16,7 +16,7 @@ namespace WorldSurveyKit.Areas.HelpPage
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
-                "HelpPage_Default1",
+                "HelpPage_Default",
                 "Help/{action}/{apiId}",
                 new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
 
